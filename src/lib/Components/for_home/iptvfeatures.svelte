bb1ccf5a728c4432cc257d1065bc03e8418e3e9e
div-styl <gouhmada@gmail.com>
2023-09-08 02:17:07 +0100

    make the cards more responsive

--- src/lib/Components/for_home/iptvfeatures.svelte
+++ src/lib/Components/for_home/iptvfeatures.svelte
@@ -2,7 +2,9 @@
   import MIcon from "./icons.svelte";
 </script>
 
-<div class="bg-[#3f323e] md:h-[220px] md:w-[500px] rounded-lg m-6" >
+<main class="md:flex md:flex-row mx-2">
+  <!-- ! countries -->
+<div class="bg-[#3f323e] 2xl:h-[300px] md:h-[470px] xl:h-[350px] lg:h-[380px]  md:w-[500px] rounded-lg m-6" >
   <div class="text-white flex flex-col justify-center items-start p-4">
     <h1 class="md:text-3xl text-2xl font-bold">
       <MIcon />
@@ -16,3 +18,35 @@
     </p>
   </div>
 </div>
+
+<!-- ! Refund -->
+<div class="bg-[#3f323e] 2xl:h-[300px] md:h-[470px] lg:h-[380px] xl:h-[350px] md:w-[500px] rounded-lg m-6" >
+  <div class="text-white flex flex-col justify-center items-start p-4">
+    <h1 class="md:text-3xl text-2xl font-bold">
+      <MIcon /> Refund Supported <br />
+    </h1>
+    <p class="md:text-sm text-xs">
+      Explore TV like never before with channels from 115 countries. <br />
+      From captivating dramas to thrilling sports, we've got it all. <br />
+      Experience diverse cultures and stories from the comfort of your home.
+    </p>
+  </div>
+</div>
+
+<!-- ! Quality -->
+<div class="bg-[#3f323e] 2xl:h-[300px] xl:h-[350px] lg:h-[380px] md:h-[470px] md:w-[500px] rounded-lg m-6" >
+  <div class="text-white flex flex-col justify-center items-start p-4">
+    <h1 class="md:text-3xl text-2xl font-bold">
+      <MIcon />
+      Channels from 115 Countries <br />
+      Around the World
+    </h1>
+    <p class="md:text-sm text-xs">
+      Explore TV like never before with channels from 115 countries. <br />
+      From captivating dramas to thrilling sports, we've got it all. <br />
+      Experience diverse cultures and stories from the comfort of your home.
+    </p>
+  </div>
+</div>
+
+</main>
