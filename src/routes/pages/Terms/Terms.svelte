c269312f07770a5cb4625798a19ce86a02c74628
div-styl <gouhmada@gmail.com>
2023-09-12 02:45:09 +0100

    privacy

--- src/routes/pages/Terms/Terms.svelte
+++ src/routes/pages/Terms/Terms.svelte
@@ -1,7 +1,25 @@
+<!-- TextDisplay.svelte -->
+
 <script>
-    let hello = "Hello World from terms.svelte";
+  // You can add JavaScript logic here if needed
 </script>
 
-<main>
-    <h1 class="text-red-500 flex justify-center items-center text-">{hello}</h1>
+<main class="flex items-center justify-center h-screen">
+  <!-- Center the background box within the screen -->
+  <div class="bg-[#1c031c] text-white font-medium w-2/6 rounded-xl p-10">
+    <!-- Content -->
+    <h1 class="text-2xl font-bold">Privacy Policy for tvpro</h1>
+    <p class="text-sm">
+      At https://tvpro.netlify.app/, accessible from https://tvpro.netlify.app/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by https://tvpro.netlify.app/ and how we use it.
+    </p>
+    <br />
+    <p class="text-sm">
+      If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us. Our Privacy Policy was generated with the help of <a class="text-blue-500" href="https://www.gdprprivacynotice.com/">GDPR Privacy Policy Generator</a>
+    </p>
+  </div>
 </main>
+
+<style>
+  /* You can add additional custom styles here if needed */
+</style>
+
