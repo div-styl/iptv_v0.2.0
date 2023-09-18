9f35b4f152376282bcfee7cb6921f76df93601aa
div-styl <gouhmada@gmail.com>
2023-09-18 23:52:56 +0100

    the hum menu is fixed so far

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-[![Netlify Status](https://api.netlify.com/api/v1/badges/f66beab0-6593-495b-aa82-d84fbcab1d50/deploy-status)](https://app.netlify.com/sites/tvpro/deploys)
 # IPTV
 
-this is a simple humble website for learning purposes to new framework called `svelte` and styling it using `tailwindcss` 
+this is a simple humble website for learning purposes to new framework called `svelte` and styling it using `tailwindcss`
 
 # How to run
-
 
 ```bash
 npm i 
 npm run dev
 ```
+
 this project is contributed
--- src/lib/Components/top_bar/mobilebar.svelte
+++ src/lib/Components/top_bar/mobilebar.svelte
@@ -2,21 +2,23 @@
   import { Link } from "svelte-routing";
   import Hum from "./humber.svelte";
   let isNavOpen = false;
+  
   const toggleNavBar = () => {
     isNavOpen = !isNavOpen;
   };
 </script>
 
-<!-- ! button menu for mobile only for 768px and below humbugger menu -->
+<!-- Hamburger menu button for mobile (768px and below) -->
 <button
   on:click={toggleNavBar}
   class="flex flex-col justify-between items-center md:hidden right-5 absolute z-[1000]"
 >
-  <Hum />
+  <!-- Include the 'open' prop to pass the state to the Hum component -->
+  <Hum open={isNavOpen} />
 </button>
 
 <div
-  class={`bg-[#0f010f]  transition ${
+  class={`bg-[#0f010f] transition ${
     isNavOpen ? "showFromRight" : "-translate-x-full"
   } fixed w-screen h-screen top-0 left-0 flex flex-col 
   justify-center items-center md:hidden z-[999] text-white text-opacity-80 font-nicomoji`}
@@ -26,31 +28,28 @@
       <Link
         to="/"
         class="block p-2 transition duration-300 ease-in-out hover:text-blue-600/60 hover:underline text-2xl"
-        
         on:click={toggleNavBar}
       >
         Home
-    </Link>
+      </Link>
     </li>
     <li>
       <Link
         to="/Contact"
         class="block p-2 transition duration-300 ease-in-out hover:text-blue-600/60 hover:underline text-2xl"
-        
         on:click={toggleNavBar}
       >
         Contact
-    </Link>
+      </Link>
     </li>
     <li>
       <Link
         to="/About"
         class="block p-2 transition duration-300 ease-in-out hover:text-blue-600/60 hover:underline text-2xl"
-        
         on:click={toggleNavBar}
       >
         About
-    </Link>
+      </Link>
     </li>
   </ul>
 </div>
