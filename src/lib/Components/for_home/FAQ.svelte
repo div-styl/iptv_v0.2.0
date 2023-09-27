6a3a1488c7349e042a91a601233c5407b93fb261
div-styl <gouhmada@gmail.com>
2023-09-27 21:46:48 +0100

    first FAQ

---
+++ src/lib/Components/for_home/FAQ.svelte
@@ -0,0 +1,45 @@
+<h1
+  class="md:text-4xl text-2xl flex justify-center items-center text-white font-extrabold"
+>
+ FAQ
+</h1>
+
+<div
+  class="relative mt-10 w-[500px]  rounded-lg bg-[#1c031c] text-white  shadow-xl
+     sm:mx-auto md:max-w-2xl sm:max-w-sm sm:rounded-lg sm:px-5 "
+>
+  <div class="mx-auto px-5">
+    <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+      <div class="py-5">
+        <details class="group">
+          <summary
+            class="flex cursor-pointer list-none items-center justify-between font-medium"
+          >
+            <span> How does the billing work?</span>
+            <span class="transition group-open:rotate-180">
+              <svg
+                fill="none"
+                height="24"
+                shape-rendering="geometricPrecision"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                viewBox="0 0 24 24"
+                width="24"
+              >
+                <path d="M6 9l6 6 6-6" />
+              </svg>
+            </span>
+          </summary>
+          <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+            Springerdata offers a variety of billing options, including monthly
+            and annual subscription plans, as well as pay-as-you-go pricing for
+            certain services. Payment is typically made through a credit card or
+            other secure online payment method.
+          </p>
+        </details>
+      </div>
+    </div>
+  </div>
+</div>
