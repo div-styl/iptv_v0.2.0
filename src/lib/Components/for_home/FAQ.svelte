6ae4683ea6c6d4e9bdb18a3289c0f1d3de8593c0
div-styl <gouhmada@gmail.com>
2023-10-01 23:01:19 +0100

    refund FAQ

--- src/lib/Components/for_home/FAQ.svelte
+++ src/lib/Components/for_home/FAQ.svelte
@@ -4,6 +4,60 @@
   FAQ
 </h1>
 <main class="m-5 -space-y-0.5">
+
+  <div
+    class="relative mt-10 max-w-lg mx-auto rounded-lg bg-[#1c031c] text-white shadow-xl
+  md:hover:ring-1 md:hover:ring-orange-500/70
+  md:w-full sm:rounded-lg sm:px-5"
+  >
+    <div class="mx-auto px-5">
+      <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+        <div class="py-5">
+          <details class="group">
+            <summary
+              class="flex cursor-pointer list-none items-center justify-between font-medium"
+            >
+              <span class="font-semibold"> How open A refund claim </span>
+              <span class="transition group-open:rotate-180">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6" />
+                </svg>
+              </span>
+            </summary>
+            <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+              To open a refuns claimer you have to reach us via Whatsapp using the keyword 
+              and reach me through Whatsapp use the keyword <span
+              class="font-bold text-orange-500"
+            >
+              <q> I Wanna Refund </q></span
+            > If the refund claimer is reasonable the refund will be processed within 24 hours.
+            </p>
+          </details>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+  <!-- !2nd v2 -->
   <div
     class="relative mt-10 max-w-lg mx-auto rounded-lg bg-[#1c031c] text-white shadow-xl
       md:hover:ring-1 md:hover:ring-orange-500/70
