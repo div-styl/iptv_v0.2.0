7f420774fadbfb637efb1ea4cfa15540e811143b
div-styl <gouhmada@gmail.com>
2023-10-01 20:30:55 +0100

    add new FQA

--- src/lib/Components/for_home/FAQ.svelte
+++ src/lib/Components/for_home/FAQ.svelte
@@ -3,44 +3,219 @@
 >
   FAQ
 </h1>
-
-<div
-  class="relative mt-10 max-w-lg mx-auto rounded-lg bg-[#1c031c] text-white shadow-xl
+<main class="m-5 -space-y-0.5">
+  <div
+    class="relative mt-10 max-w-lg mx-auto rounded-lg bg-[#1c031c] text-white shadow-xl
       md:hover:ring-1 md:hover:ring-orange-500/70
       md:w-full sm:rounded-lg sm:px-5"
->
-  <div class="mx-auto px-5">
-    <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
-      <div class="py-5">
-        <details class="group">
-          <summary
-            class="flex cursor-pointer list-none items-center justify-between font-medium"
-          >
-            <span> How does the billing work?</span>
-            <span class="transition group-open:rotate-180">
-              <svg
-                fill="none"
-                height="24"
-                shape-rendering="geometricPrecision"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1.5"
-                viewBox="0 0 24 24"
-                width="24"
-              >
-                <path d="M6 9l6 6 6-6" />
-              </svg>
-            </span>
-          </summary>
-          <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
-            Springerdata offers a variety of billing options, including monthly
-            and annual subscription plans, as well as pay-as-you-go pricing for
-            certain services. Payment is typically made through a credit card or
-            other secure online payment method.
-          </p>
-        </details>
+  >
+    <div class="mx-auto px-5">
+      <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+        <div class="py-5">
+          <details class="group">
+            <summary
+              class="flex cursor-pointer list-none items-center justify-between font-medium"
+            >
+              <span class="font-semibold"> Available Payment Options</span>
+              <span class="transition group-open:rotate-180">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6" />
+                </svg>
+              </span>
+            </summary>
+            <ul class="group-open:animate-fadeIn mt-3 text-neutral-600">
+              <li>PayPal</li>
+              <li>Credit Card</li>
+              <li>Crypto</li>
+            </ul>
+          </details>
+        </div>
       </div>
     </div>
   </div>
-</div>
+
+  <!-- !2nd FAQ -->
+
+  <div
+    class="relative mt-10 max-w-lg mx-auto rounded-lg bg-[#1c031c] text-white shadow-xl
+      md:hover:ring-1 md:hover:ring-orange-500/70
+      md:w-full sm:rounded-lg sm:px-5"
+  >
+    <div class="mx-auto px-5">
+      <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+        <div class="py-5">
+          <details class="group">
+            <summary
+              class="flex cursor-pointer list-none items-center justify-between font-medium"
+            >
+              <span class="font-semibold"> Which devices are supported</span>
+              <span class="transition group-open:rotate-180">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6" />
+                </svg>
+              </span>
+            </summary>
+            <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+              Our IPTV service works on any device such smart Tv(LG, Sony,
+              Samsung) that has a good access to the internet connection
+            </p>
+          </details>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ! 3rf FAQ  -->
+
+  <div
+    class="relative mt-10 max-w-lg mx-auto rounded-lg bg-[#1c031c] text-white shadow-xl
+  md:hover:ring-1 md:hover:ring-orange-500/70
+  md:w-full sm:rounded-lg sm:px-5"
+  >
+    <div class="mx-auto px-5">
+      <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+        <div class="py-5">
+          <details class="group">
+            <summary
+              class="flex cursor-pointer list-none items-center justify-between font-medium"
+            >
+              <span class="font-semibold"> Recommanded Internet Speed</span>
+              <span class="transition group-open:rotate-180">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6" />
+                </svg>
+              </span>
+            </summary>
+            <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+              if your downloading speed is not less than 30mbps, then everything
+              will be fine. In case you are not sure ask for 1 day trial and
+              test it.
+            </p>
+          </details>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ! 4th FAQ  -->
+
+  <div
+    class="relative mt-10 max-w-lg mx-auto rounded-lg bg-[#1c031c] text-white shadow-xl
+  md:hover:ring-1 md:hover:ring-orange-500/70
+  md:w-full sm:rounded-lg sm:px-5"
+  >
+    <div class="mx-auto px-5">
+      <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+        <div class="py-5">
+          <details class="group">
+            <summary
+              class="flex cursor-pointer list-none items-center justify-between font-medium"
+            >
+              <span class="font-semibold">
+                Can I use the IPTV in many devices</span
+              >
+              <span class="transition group-open:rotate-180">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6" />
+                </svg>
+              </span>
+            </summary>
+            <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+              Of course it is allowed to use the IPTV Login at multiple devices,
+              but be aware that it work at one at the time only.
+            </p>
+          </details>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ! 4th FAQ  -->
+
+  <div
+    class="relative mt-10 max-w-lg mx-auto rounded-lg bg-[#1c031c] text-white shadow-xl
+  md:hover:ring-1 md:hover:ring-orange-500/70
+  md:w-full sm:rounded-lg sm:px-5"
+  >
+    <div class="mx-auto px-5">
+      <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+        <div class="py-5">
+          <details class="group">
+            <summary
+              class="flex cursor-pointer list-none items-center justify-between font-medium"
+            >
+              <span class="font-semibold"> How can I get my trial</span>
+              <span class="transition group-open:rotate-180">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6" />
+                </svg>
+              </span>
+            </summary>
+            <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+              scroll through the website and click on the button that says <span
+                class="font-bold text-orange-500"
+              >
+                <q> Free Trail </q></span
+              >
+              and reach me through Whatsapp use the keyword <span
+              class="font-bold text-orange-500"
+            >
+              <q> I need a trial</q></span
+            >
+            </p>
+          </details>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
