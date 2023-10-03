6f6c0c3a51a000ec577c9f05487481e61fd53e40
div-styl <gouhmada@gmail.com>
2023-10-03 12:42:54 +0100

    use local host

--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: ['http://nexgenstream.me', 'https://nexgenstream.me'],
+  origin: ['http://nexgenstream.me', 'https://nexgenstream.me', 'http://localhost:5173/'],
   credentials: true,
   optionsSuccessStatus: 200 
 }));
